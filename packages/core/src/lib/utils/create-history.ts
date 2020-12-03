dfeb9d2822e45ba2353d3a52cdfa1dde489ab5c7
nabos.dezigner@gmail.com <0o9i8u7yFill>
2020-12-03 20:40:40 +0300

    feat: refactoring history state

--- packages/core/src/lib/utils/create-history.ts
+++ packages/core/src/lib/utils/create-history.ts
@@ -1,14 +1,25 @@
 import {createEvents} from "./create-events";
 
-type THistoryEvent = (location: Location[]) => void;
+type THistory = {
+  pathname: string
+  search: string
+  hash: string
+}
+
+type THistoryEvent = (location: THistory[]) => void;
+
+const getLocation = (): THistory => {
+  const {pathname, search, hash} = window.location;
+  return {pathname, search, hash}
+}
 
 export const createHistory = () => {
-  let context: Location[] = [window.location];
+  let context: THistory[] = [getLocation()];
 
   const listener = createEvents<THistoryEvent>();
 
   window.onpopstate = () => {
-    context.push(window.location);
+    context.push(getLocation());
     listener.call(context)
   }
 
