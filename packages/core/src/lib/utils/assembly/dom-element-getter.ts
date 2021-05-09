0fac2d4636732badc21f0968146e122966998259
Naboska <nabos.dezigner@gmail.com>
2021-05-09 22:03:24 +0300

    feat: root module element getter

---
+++ packages/core/src/lib/utils/assembly/dom-element-getter.ts
@@ -0,0 +1,23 @@
+import { IAppProps } from "lib";
+
+const createModuleNode = (name: string) => {
+  const node = document.createElement('div');
+  node.id = name;
+
+  return node;
+};
+
+export const domElementGetter = ({ name }: IAppProps): Element => {
+  const findNode = document.querySelector(`div[id$='${name}']`);
+
+  if (!findNode) {
+    const root = document.body;
+    const moduleNode = createModuleNode(name);
+
+    root.appendChild(moduleNode);
+
+    return moduleNode;
+  }
+
+  return findNode;
+}
