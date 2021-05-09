e5b0644797b29f9ac8ea4fdcd110f67b8f8afc99
Naboska <nabos.dezigner@gmail.com>
2021-05-09 22:03:37 +0300

    feat: import module getter

---
+++ packages/core/src/lib/utils/assembly/import-module.ts
@@ -0,0 +1,14 @@
+import { CustomProps, LifeCycles } from "single-spa";
+
+import { IAppProps, SystemJs } from "lib";
+import { domElementGetter } from "./dom-element-getter";
+
+export const importModule = <T extends CustomProps>(config: IAppProps): Promise<LifeCycles<T>> => {
+  const moduleNode = domElementGetter(config);
+  const loaderNode: HTMLElement = document.createElement('div');
+  loaderNode.innerText = 'loading';
+
+  moduleNode.appendChild(loaderNode);
+
+  return SystemJs.import<LifeCycles<T>>(config.name);
+}
